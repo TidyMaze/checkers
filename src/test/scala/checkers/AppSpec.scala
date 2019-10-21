a2fe05b27a3d7c22c0f1e2919815dc9c55fc3403
Yann <groskiff@gmail.com>
2019-10-21 21:22:54 +0200

    cannot jump same player

--- src/test/scala/checkers/AppSpec.scala
+++ src/test/scala/checkers/AppSpec.scala
@@ -3,7 +3,5 @@ package checkers
 import org.scalatest._
 
 class AppSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    App.greeting shouldEqual "hello"
-  }
+
 }
