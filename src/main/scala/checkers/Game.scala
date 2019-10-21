50e94a806603f4ad175598fa646806fb6b76ae88
Yann <groskiff@gmail.com>
2019-10-21 20:51:17 +0200

    play till end with winner

--- src/main/scala/checkers/Game.scala
+++ src/main/scala/checkers/Game.scala
@@ -4,10 +4,10 @@ import checkers.Coord.add
 import checkers.Direction.toOffset
 import checkers.Grid.{Grid, HEIGHT, WIDTH, update2D}
 import checkers.Player.{Player1, Player2, nextPlayer}
+import checkers.RandomHelpers.shuffle
 
+import scala.annotation.tailrec
 import scala.util.{Failure, Success, Try}
-
-import RandomHelpers.shuffle
 
 object Game {
 
@@ -23,35 +23,75 @@ object Game {
     )
   }
 
-  def isInGrid(coord: Coord): Boolean = coord.y >= 0 && coord.x >= 0 && coord.y < HEIGHT && coord.x < WIDTH
+  def playSeveralTurnsRandomly(state: State, turns: Int): List[State] =
+    playSeveralTurnsWithEvalFunction(state, turns, _ => 0)
 
-  def playAction(action: Action, state: State): Try[State] = {
-    val destCoord = add(action.from, toOffset(action.direction))
+  def playSeveralTurnsWithEvalFunction(state: State, turns: Int, eval: State => Double): List[State] =
+    playTillEndWithEvalFunction(state, eval).take(turns)
 
-    if (!isInGrid(destCoord)) {
-      Failure(new RuntimeException("Invalid coord outside of grid"))
-    } else if (state.grid(destCoord.y)(destCoord.x).isDefined) {
-      Failure(new RuntimeException("Invalid coord already occupied"))
-    } else {
-      val resGrid = move(state.grid, action.from, destCoord, state.nextPlayer)
-      Success(State(resGrid, nextPlayer(state.nextPlayer), state.winner))
+  def playTillEndWithEvalFunction(state: State, eval: State => Double): List[State] = {
+    @tailrec
+    def aux(s: State, acc: List[State]): List[State] = {
+      val actions = findAllActions(s)
+      if (actions.isEmpty) {
+        acc.reverse
+      } else {
+        val (_, newState) = shuffle(actions.toSeq).maxBy { case (_, state) => eval(state) }
+        aux(newState, newState +: acc)
+      }
     }
+
+    aux(state, Nil)
   }
 
-  def move(grid: Grid, from: Coord, to: Coord, player: Player): Grid = update2D(update2D(grid, from, None), to, Some(player))
-
   def findAllActions(state: State): Map[Action, State] = {
-    val player = state.nextPlayer
-    val playerPieces = findPiecesCoords(state.grid)(player)
-
     (for {
-      from <- playerPieces
+      from <- findPiecesCoords(state.grid).getOrElse(state.nextPlayer, Nil)
       dir <- Direction.values
       action = Action(from, dir)
       maybeResState = playAction(action, state).toOption
       resState <- maybeResState if maybeResState.isDefined
     } yield action -> resState).toMap
   }
+
+  def playAction(action: Action, state: State): Try[State] = {
+    val offset = toOffset(action.direction)
+    val destCoord = add(action.from, offset)
+
+    if (!isInGrid(destCoord)) {
+      Failure(new RuntimeException("Invalid coord outside of grid"))
+    } else if (state.grid(destCoord.y)(destCoord.x).isDefined) {
+      val destCoord2 = add(destCoord, offset)
+      if (!isInGrid(destCoord2)) {
+        Failure(new RuntimeException("Invalid coord outside of grid"))
+      } else if (state.grid(destCoord2.y)(destCoord2.x).isDefined) {
+        Failure(new RuntimeException("Invalid coord already occupied"))
+      } else {
+        val resGrid2 = jump(state.grid, action.from, destCoord, destCoord2, state.nextPlayer)
+        Success(State(resGrid2, nextPlayer(state.nextPlayer), winner(resGrid2)))
+      }
+    } else {
+      val resGrid = move(state.grid, action.from, destCoord, state.nextPlayer)
+      Success(State(resGrid, nextPlayer(state.nextPlayer), winner(resGrid)))
+    }
+  }
+
+  def isInGrid(coord: Coord): Boolean = coord.y >= 0 && coord.x >= 0 && coord.y < HEIGHT && coord.x < WIDTH
+
+  def winner(grid: Grid): Option[Player] = {
+    val playersPieces = findPiecesCoords(grid)
+    if (!playersPieces.isDefinedAt(Player1)) Some(Player2)
+    else if (!playersPieces.isDefinedAt(Player2)) Some(Player1)
+    else None
+  }
+
+  def move(grid: Grid, from: Coord, to: Coord, player: Player): Grid = update2D(update2D(grid, from, None), to, Some(player))
+
+  def jump(grid: Grid, from: Coord, jumped: Coord, to: Coord, player: Player): Grid = {
+    eat(move(grid, from, to, player), jumped)
+  }
+
+  def eat(grid: Grid, coord: Coord): Grid = update2D(grid, coord, None)
 
   def findPiecesCoords(grid: Grid): Map[Player, Seq[Coord]] = {
     (for {
@@ -65,24 +105,8 @@ object Game {
       .mapValues(_.map { case (_, coord) => coord })
   }
 
-  def playSeveralTurnsRandomly(state: State, turns: Int): Seq[State] = playSeveralTurnsWithEvalFunction(state, turns, _ => 0)
-
-  def playSeveralTurnsWithEvalFunction(state: State, turns: Int, eval: State => Double): Seq[State] = {
-    turns match {
-      case 0 => Nil
-      case remainingTurns =>
-        val actions = findAllActions(state)
-        if (actions.isEmpty) {
-          State(state.grid, state.nextPlayer, Some(nextPlayer(state.nextPlayer))) :: Nil
-        } else {
-          val (_, newState) = shuffle(actions.toSeq).maxBy { case (_, state) => eval(state)}
-          newState +: playSeveralTurnsRandomly(newState, remainingTurns - 1)
-        }
-    }
-  }
-
   def basicEvalFunction(state: State): Double = {
     val pieces = Game.findPiecesCoords(state.grid)
-    pieces(state.nextPlayer).size - pieces(Player.nextPlayer(state.nextPlayer)).size
+    pieces.getOrElse(state.nextPlayer, Nil).size - pieces.getOrElse(Player.nextPlayer(state.nextPlayer), Nil).size
   }
 }
