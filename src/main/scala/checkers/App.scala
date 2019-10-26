cabfcbd8622102a5141b0f57c8db05fbf9ed7cd6
Yann <groskiff@gmail.com>
2019-10-26 21:27:29 +0200

    cleanup

--- src/main/scala/checkers/App.scala
+++ src/main/scala/checkers/App.scala
@@ -4,17 +4,6 @@ import checkers.Game._
 
 object App extends App {
   val state: State = Game.newGame()
-  println(state)
-  println()
-  val actions = Game.findAllActions(state)
-  println(s"${actions.size} actions:")
-  println(actions.map{ case (action, _) => action}.mkString("\n"))
-  println()
-
-  println(Grid.printableGrids(actions.map {
-    case (_, resState) => resState.grid
-  }.toList))
-  println()
 
   val turnHandler: (Action, State, Double) => Unit = (action, currentState, score) => {
     println()
@@ -32,15 +21,4 @@ object App extends App {
   val endGameWinner: Option[String] = states.lastOption.flatMap(_.winner.map(_.toString))
   println(s"winner is ${endGameWinner.getOrElse("nobody")}")
 
-//  val allSizes = (0 until 100) map {iGame =>
-//    val state: State = Game.newGame()
-//    val history = playTillEndWithEvalFunction(state, basicEvalFunction)
-//    history.size
-//  }
-//
-//  val min = allSizes.min
-//  val avg = average(allSizes)
-//  val max = allSizes.max
-//
-//  println(s"Game stats over ${allSizes.size} samples: $min - $avg - $max")
 }
