e5e28f2a85558b618db20f593f29014bbf206992
Yann <groskiff@gmail.com>
2019-10-27 10:28:25 +0100

    fast shuffle

--- src/main/scala/checkers/RandomHelpers.scala
+++ src/main/scala/checkers/RandomHelpers.scala
@@ -1,5 +1,6 @@
 package checkers
 
+import scala.collection.mutable.ArrayBuffer
 import scala.util.{Failure, Random, Success, Try}
 
 object RandomHelpers {
@@ -10,5 +11,18 @@ object RandomHelpers {
     else Success(arr(random.nextInt(arr.size)))
 
   def randPct(): Double = random.nextDouble()
+
+  def shuffle[A](xs: ArrayBuffer[A]): Unit = {
+    def swap(i1: Int, i2: Int) {
+      val tmp = xs(i1)
+      xs(i1) = xs(i2)
+      xs(i2) = tmp
+    }
+
+    for (n <- xs.length to 2 by -1) {
+      val k = random.nextInt(n)
+      swap(n - 1, k)
+    }
+  }
 
 }
