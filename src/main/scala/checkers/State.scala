12dc3256450fd0f01878aa4e56ef72eb9cd376af
Yann <groskiff@gmail.com>
2019-10-27 10:38:49 +0100

    optim pieces looku

--- src/main/scala/checkers/State.scala
+++ src/main/scala/checkers/State.scala
@@ -3,7 +3,7 @@ package checkers
 import checkers.Game.Player
 import checkers.Grid.{Grid, asPrintable2DArray}
 
-case class State(grid: Grid, nextPlayer: Player, winner: Option[Player]) {
+case class State(grid: Grid, nextPlayer: Player, playersPieces: Map[Player, Seq[Coord]], winner: Option[Player]) {
   override def toString: String = asPrintable2DArray(grid).map(_.mkString(" ")).mkString("\n")
 }
 
